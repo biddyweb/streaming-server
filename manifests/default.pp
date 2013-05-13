0539db2eca078542f63168ba3a1be0493b785eb5
Emilio Blanco <emilioeduardob@gmail.com>
2013-05-13 10:30:05 -0400

    Changed puppet to install dependencies

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,23 +1,21 @@
 exec { "apt-get update":
-  path => "/usr/bin",
-}
-package { "python-software-properties":
-  ensure  => present,
-  require => Exec["apt-get update"],
-}
-package { "software-properties-common":
-  ensure  => present,
-  require => Exec["apt-get update"],
+  path => "/usr/bin"
 }
 
-exec { "add-apt-repository":
+package { 'python-software-properties':
+  ensure  => installed,
+  require  => Exec['apt-get update'],
+}
+
+exec { "node.js-legacy":
   command   => "/usr/bin/add-apt-repository ppa:chris-lea/node.js-legacy",
-   notify    => Exec['apt-get update']
- }
+  require => Package["python-software-properties"],
+}
 
- $lynckia_reqs = ['git', 'make', 'gcc', 'g++', 'libssl-dev', 'cmake', 'libnice10', 'libnice-dev', 'libglib2.0-dev', 'pkg-config', 'nodejs', 'nodejs-dev', 'npm', 'libboost-regex-dev', 'libboost-thread-dev', 'libboost-system-dev', 'rabbitmq-server', 'mongodb', 'openjdk-6-jre']
+$lynckia_reqs = ['git', 'make', 'gcc', 'g++', 'libssl-dev', 'cmake', 'libnice10', 'libnice-dev', 'libglib2.0-dev', 'pkg-config', 'nodejs', 'nodejs-dev', 'npm', 'libboost-regex-dev', 'libboost-thread-dev', 'libboost-system-dev', 'rabbitmq-server', 'mongodb', 'openjdk-6-jre']
 
 package { $lynckia_reqs:
   ensure => installed,
-  require => Exec["add-apt-repository"]
+  require => Exec["node.js-legacy"]
 }
+
